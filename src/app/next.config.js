d0fdb778c7aef30a87a2dfbff050da3c0b2396bc
Kentaro Matsushita <kentaro.m811@gmail.com>
2018-10-05 01:39:41 +0900

    Add a typescript configuration

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -3,26 +3,29 @@ require('dotenv').config()
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 const withSass = require('@zeit/next-sass')
+const withTypescript = require('@zeit/next-typescript')
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.md?$/
 })
 
 module.exports = withMDX(
-  withSass({
-    distDir: '../../dist/functions/next',
-    webpack(config) {
-      config.plugins = config.plugins || []
+  withTypescript(
+    withSass({
+      distDir: '../../dist/functions/next',
+      webpack(config) {
+        config.plugins = config.plugins || []
 
-      config.plugins = [
-        ...config.plugins,
+        config.plugins = [
+          ...config.plugins,
 
-        new Dotenv({
-          path: path.join(__dirname, '.env'),
-          systemvars: true
-        })
-      ]
+          new Dotenv({
+            path: path.join(__dirname, '.env'),
+            systemvars: true
+          })
+        ]
 
-      return config
-    }
-  })
+        return config
+      }
+    })
+  )
 )
